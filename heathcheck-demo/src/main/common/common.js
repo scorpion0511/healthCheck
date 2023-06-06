cc4ae3e7c300d14ac4aad2a4ac6a7fd22e1fb54b
sherif sadek <sherif_h_sadek@yahoo.com>
2023-06-06 13:05:27 -0300

    m

--- heathcheck-demo/src/main/common/common.js
+++ heathcheck-demo/src/main/common/common.js
@@ -4,26 +4,33 @@ export const pods = ['Panorama-Cloud', 'MESSAGE-CLOUD', 'LAB-Web-SERVICE','NVENT
   'BATCH-SCHEDULER-INVENTORY','BATCH-SCHEDULER-IMMUNIZATION',
     '', ''];
 
- export const podsObjs = {'Panorama-Cloud':'panorama-cloud', 'MESSAGE-CLOUD':'messaging-cloud', 'LAB-Web-SERVICE':'lab-web-services-web','NVENTORY-WEB-SERVICE':'inventory-web-services-web', 'BATCH-SCHEDULER-CASE':'batch-scheduler-case',
+ const podsObjs = {'Panorama-Cloud':'panorama-cloud', 'MESSAGE-CLOUD':'messaging-cloud', 'LAB-Web-SERVICE':'lab-web-services-web','NVENTORY-WEB-SERVICE':'inventory-web-services-web', 'BATCH-SCHEDULER-CASE':'batch-scheduler-case',
   'BATCH-SCHEDULER-INVENTORY':'batch-scheduler-inventory','BATCH-SCHEDULER-IMMUNIZATION':'batch-scheduler-immunization', 'INVESTIGATION-API':'sdsmapi-investigation-web', 'CLIENT-API':'sdsmapi-client-web',
   'TERMINOLOGY-API':'sdsmapi-terminology-web', 'INVENTORY-API':'sdsmapi-inventory-web', 'CONSENT-API':'sdsmapi-consent-web', 'ORGANIZATION-API':'sdsmapi-organization-web',
   'SECURITY-API':'sdsmapi-security-web' , 'PROVIDER-API':'sdsmapi-provider-web',
   'IMMUNIZATION-API':'sdsmapi-immunization-web' , 'SHARED-SERVICE-API':'sdsmapi-sharedservice-web',
    'Reporting-Business-Intelligence':'reporting-business-intelligence'};
 
-   export const link = 'panorama.ocp-prime-c55d62f44ecad82c5f9640d22d3526e6-i000.ca-tor.containers.appdomain.cloud/health/live';
-   export const separation = '-';
-   export const protocol = 'http://';
-   export const envs = ['', 'T1', 'T2', 'AM1', 'AM3', 'AM4'];
+   const link = 'panorama.ocp-prime-c55d62f44ecad82c5f9640d22d3526e6-i000.ca-tor.containers.appdomain.cloud/health/live';
+   const separation = '-';
+   const protocol = 'http://';
+   export const envs = ['', 'T1', 'T2', 'AM1', 'AM3', 'AM4', 'PRT1'];
+   const PRT1_PANORAMA_CLOUD = "https://panorama-prt1-panorama.ocp-prime-c55d62f44ecad82c5f9640d22d3526e6-i000.ca-tor.containers.appdomain.cloud/health/ready";
 
   export const buildUrl = (podId, env) => {
+    const tempLink = link;
     if (podId.length > 0)
     {
         let httpProtocol = protocol;
         if (env.toUpperCase() === 'AM1' && podId === 'Panorama-Cloud')
        {
         httpProtocol = 'https://';
+      
        } 
+       else if (env.toUpperCase() === 'PRT1' && podId === 'Panorama-Cloud')
+       {
+        return PRT1_PANORAMA_CLOUD;
+       }
         return httpProtocol + podsObjs[podId] + separation + env + separation + link;
     }
     return podId;
